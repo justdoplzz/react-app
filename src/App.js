7d8359b729736c5dd95da5ee98977c977d865676
khs <khs1305@dreamsecurity.com>
2023-10-30 14:49:14 +0900

    [session3] state - key 설정 - key 는 react가 원하는 값으로써 그렇구나~하고 설정해주면 됨

--- src/App.js
+++ src/App.js
@@ -9,18 +9,23 @@ class App extends Component {
   constructor(props){        // render()보다 먼저 실행되면서 component 값 초기화
     super(props);
     this.state = {          // 내부적으로 사용할 때 state 로 사용
-      subject:{title:'WEB', sub:'world wide web!'}
+      subject: {title:'WEB', sub:'world wide web!'},
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
     }
   }
   render(){
     return (
       <div className="App">
-        <Subject 
-          title={this.state.subject.title} 
+        <Subject
+          title={this.state.subject.title}
           sub={this.state.subject.sub}>
         </Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
