ace81666f48d4d3b26290ad06095b45364b87d22
khs <khs1305@dreamsecurity.com>
2023-10-30 14:32:24 +0900

    [session3] state 기초 사용 - constructor : render() 함수보다 먼저 실행되면서 component 값 초기화

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import TOC from "./components/TOC";
 
 
 class App extends Component {
+  constructor(props){        // render()보다 먼저 실행되면서 component 값 초기화
+    super(props);
+    this.state = {          // 내부적으로 사용할 때 state 로 사용
+      subject:{title:'WEB', sub:'world wide web!'}
+    }
+  }
   render(){
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <Subject title="React" sub="For UI"></Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
