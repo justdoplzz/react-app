c62b7989270f7535866583a88d775d13a8e6f174
khs <khs1305@dreamsecurity.com>
2023-10-30 15:05:49 +0900

    [session4] 모드 설정

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
   constructor(props){        // render()보다 먼저 실행되면서 component 값 초기화
     super(props);
     this.state = {          // 내부적으로 사용할 때 state 로 사용
+      mode: 'welcome',
       subject: {title:'WEB', sub:'world wide web!'},
+      welcome: {title:'Welcome', desc: 'Hello, React!'},
       contents: [
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -18,6 +20,15 @@ class App extends Component {
     }
   }
   render(){
+    console.log("App render");
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject
@@ -26,7 +37,7 @@ class App extends Component {
         </Subject>
         <Subject title="React" sub="For UI"></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
