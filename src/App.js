e6bcf65a2d16e45e094c220ece196f988e3b2079
khs <khs1305@dreamsecurity.com>
2023-10-31 17:13:53 +0900

    복습을 위해 다시 init

--- src/App.js
+++ src/App.js
@@ -1,66 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Content from "./components/Content";
-import Subject from './components/Subject';
-import TOC from "./components/TOC";
 
 
 class App extends Component {
-  constructor(props){        // render()보다 먼저 실행되면서 component 값 초기화
-    super(props);
-    this.state = {          // 내부적으로 사용할 때 state 로 사용
-      mode: 'read',
-      selected_content_id:2,
-      subject: {title:'WEB', sub:'world wide web!'},
-      welcome: {title:'Welcome', desc: 'Hello, React!'},
-      contents: [
-        {id:1, title:'HTML', desc:'HTML is for information'},
-        {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
-      ]
-    }
-  }
   render(){
-    console.log("App render");
-    var _title, _desc = null;
-    if(this.state.mode === 'welcome'){
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-
-    } else if (this.state.mode === 'read'){
-      var i = 0;
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-    }
-    return (
+    return(
       <div className="App">
-        <Subject
-          title={this.state.subject.title}
-          sub={this.state.subject.sub}
-          onChangePage={function(){
-            this.setState({mode:'welcome'});
-          }.bind(this)}
-          >
-        </Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC
-          onChangePage={function(id){
-            this.setState({
-              mode:'read',
-              selected_content_id:Number(id)
-            });
-          }.bind(this)}
-          data={this.state.contents}></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        Hello, React!
       </div>
-    );
+    )
   }
 }
 
