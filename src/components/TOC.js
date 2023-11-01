bd3340c75d9a7db80a2979cf6b289cc7e7ce3533
khs <khs1305@dreamsecurity.com>
2023-11-01 14:32:55 +0900

    [section5] create 기능 추가구현 (shouldComponentUpdate) - shouldComponentUpdate 사용함으로서 서능 개선 - return true : render() 함수 호출 - return false : render() 함수 미호출 - 이전 push(원본변경), concat(원본미변경, 복제본에 저장) 의 차이를 여기서 알게 됨

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 
 class TOC extends Component{
+    shouldComponentUpdate(newProps, newState){      // props, state가 변경되었을 때의 값을 인자로 받음
+        console.log("===> TOC render shouldComponentUpdate"
+            ,newProps.data      // 바뀐 값
+            ,this.props.data    // 기존 값
+        );
+        if(this.props.data === newProps.data){
+            return false;       // false 인 경우 render() 함수 미호출
+        }
+        return true;            // true 인 경우 render() 함수 호출
+                            
+    }
     render(){
+        console.log("===>TOC render");
         var lists = [];
         var data = this.props.data;
         var i = 0;
