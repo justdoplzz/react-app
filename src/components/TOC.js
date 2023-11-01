ee03de6770a9ca5ad91708eebf87cf5e1b48e5d3
khs <khs1305@dreamsecurity.com>
2023-11-01 11:20:51 +0900

    [section4] 컴포넌트 이벤트 생성 및 TOC 내용 표출 - TOC 목록 선택시 해당 내용 표출

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -6,7 +6,29 @@ class TOC extends Component{
         var data = this.props.data;
         var i = 0;
         while(i < data.length){
-            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+            lists.push(
+                // 1. 인자를 이용한 방식
+                /*<li key={data[i].id}>
+                    <a href={"/content/"+data[i].id}
+                       onClick={function(id, e){
+                           e.preventDefault();
+                           this.props.onChangePage(id);
+                       }.bind(this, data[i].id)}>
+                        {data[i].title}
+                    </a>
+                </li> */
+                //  2. 속성을 이용한 방식
+                <li key={data[i].id}>
+                    <a href={"/content/"+data[i].id}
+                        data-id={data[i].id}
+                       onClick={function(e){
+                           e.preventDefault();
+                           this.props.onChangePage(e.target.dataset.id);
+                       }.bind(this)}>
+                        {data[i].title}
+                    </a>
+                </li>
+            );
             i = i + 1;
         }
       return(
