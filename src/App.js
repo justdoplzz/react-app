403a772397241dc28ec7cd40fe9f5173ffa7e3dc
khs <khs1305@dreamsecurity.com>
2023-11-01 11:33:32 +0900

    [section5] mode 변경 기능

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Content from './components/Content';
+import Control from './components/Control';
 import Subject from './components/Subject';
 import TOC from './components/TOC';
 
@@ -47,13 +48,23 @@ class App extends Component {
           }.bind(this)}>
         </Subject>
 
-        <TOC onChangePage={function(id){
+        <TOC
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+          }.bind(this)}
+          data={this.state.contents}>
+        </TOC>
+
+        <Control onChangeMode={function(_mode){
           this.setState({
-            mode:'read',
-            selected_content_id:Number(id)
+            mode:_mode
           });
-        }.bind(this)} 
-        data={this.state.contents}></TOC>
+        }.bind(this)}>
+
+        </Control>
 
         <Content title={_title} desc={_desc}></Content>
       </div>
